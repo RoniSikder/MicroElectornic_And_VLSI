264d87f526f6b909f6be0aba329af936b54f62c9
RoniSikder <ronisikder200@gmail.com>
2022-08-30 23:40:38 +0530

    eight commite

--- src/Components/Notice_Card.js
+++ src/Components/Notice_Card.js
@@ -10,14 +10,6 @@ const Notice_Card = ({ id }) => {
                     <p class="card-text" style={{ color: "green" }} >{Notice_Data[id].date}</p>
                     <p class="card-text" style={{ color: "orange" }} >{Notice_Data[id].about}</p>
                     <a href="#" class="btn btn-primary">go to details</a>
-                    <div className="card text-center" style={{ width: "30rem", margin: "2%", backgroundColor: "transparent", border: "3px solid black" }}>
-                    </div>
-                    <div className="card-body">
-                        <h3 className="card-title" style={{ color: "black", fontFamily: "'Kanit', sans-serif" }}>{Notice_Data[id].name}</h3>
-                        <p className="card-text" style={{ color: "green" }} >{Notice_Data[id].date}</p>
-                        <p className="card-text" style={{ color: "orange" }} >{Notice_Data[id].about}</p>
-                        <a href="#" className="btn btn-primary">go to details</a>
-                    </div>
                 </div>
             </div>
         </>
